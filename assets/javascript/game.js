b48bf5285687e3c398709b95e6b6bd8b42f4fa32
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2018-10-21 11:25:26 -0400

    Working file with mackup style and functionality 10.21.2018 sunday 11.26am

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+// onloaded generate a random number
+// store it in a var random nunber
+// oclick of each diamond generate random number
+// add the number to a variable 
+// continue adding the random numbers if the total is less than generated number
+// if the total generated number is greater than the orig random number you loose
+// increase the value of lose by 1
+// if the total generated number is equal to the random generated number, you win
+// increase the value of win by 1
+// on refresh, reset the game
+
+// randomly generated number
+let randomNumber = Math.floor((Math.random() * 120) + 18);
+let redGemStone = Math.floor((Math.random() * 12) + 1);
+let blueGemStone = Math.floor((Math.random() * 12) + 1);
+let purpleGemStone = Math.floor((Math.random() * 12) + 1);
+let greenGemSone = Math.floor((Math.random() * 12) + 1);
+let randomNumberDisplay = $(".section__game--randomnumberDisplay");
+let totalScore = 0;
+let win = 0;
+let winDisplay = $(".section__game--outcomeWin");
+let loose = 0;
+let looseDisplay = $(".section__game--outcomeLoose");
+let scoreKeeper = $(".section__score--displayKeeper");
+
+$(document).ready(function(){
+    // red gem event handler
+    randomNumberDisplay.text(randomNumber);
+    $(".section__gems--red").on("click",function(){
+        totalScore += redGemStone;
+        gameChecker();
+    });
+     // blue gem event handler
+    $(".section__gems--blue").on("click",function(){
+        totalScore += blueGemStone;
+        gameChecker();
+    });
+     // purple gem event handler
+    $(".section__gems--purple").on("click",function(){
+        totalScore += purpleGemStone;
+        gameChecker();
+    });
+     // green gem event handler
+    $(".section__gems--green").on("click",function(){
+        totalScore += greenGemSone;
+        gameChecker();
+    });
+});
+
+let gameChecker = ()=>{
+    scoreKeeper.text(totalScore);
+        if(randomNumber===totalScore){
+            win++; 
+            winDisplay.text(win); 
+            randomNumber = Math.floor((Math.random() * 120) + 18);
+            totalScore = 0;
+            scoreKeeper.text(totalScore);
+        }else if(totalScore > randomNumber){
+            loose++;
+            looseDisplay.text(loose);
+            randomNumber = Math.floor((Math.random() * 120) + 18);
+            totalScore = 0;
+            scoreKeeper.text(totalScore);
+        }
+};
+
+
